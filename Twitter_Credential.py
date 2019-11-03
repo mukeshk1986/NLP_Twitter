e770325cb582aa34d59808e8131851da196803a0
Mukesh Kumar <42538599+mukeshk1986@users.noreply.github.com>
2019-11-03 15:33:07 -0800

    Create Twitter_Credential.py
    Credential to use Twitter API

---
+++ Twitter_Credential.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 21 09:31:07 2019
+
+@author: m48kumar
+"""
+#apikey='1lg7MvlLx0UBouMjEipujmmrD',
+#apisecretkey='gZssakJNagGwVkHVLglE4shfPoHU4Y1vVT9vb0U3sl1GNwM7wH'
+#accesstoken='224245933-Mqd8aCNimnor93PBdB366xE7o9lKJLxO6QpiWZsX'
+#accesstokensecret='Eb2zi7nVCpZZpYIeHq284XKQiKJDHHS4UwJhUqeQo0Z2d'
+apikey='1lg7MvlLx0UBouMjEipujmmrD'
+apisecretkey='gZssakJNagGwVkHVLglE4shfPoHU4Y1vVT9vb0U3sl1GNwM7wH'
+accesstoken='224245933-Mqd8aCNimnor93PBdB366xE7o9lKJLxO6QpiWZsX'
+accesstokensecret='Eb2zi7nVCpZZpYIeHq284XKQiKJDHHS4UwJhUqeQo0Z2d'
+print("Hello")
